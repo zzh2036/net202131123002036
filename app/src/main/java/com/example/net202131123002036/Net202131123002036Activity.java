20c9c5398de424b6f36ca9dbd42efa750b24c5c8
Zouzihao666 <1917636709@qq.com>
2024-03-11 00:35:43 +0800

    First experiment of Android studio-Hello world

---
+++ app/src/main/java/com/example/net202131123002036/Net202131123002036Activity.java
@@ -0,0 +1,14 @@
+package com.example.net202131123002036;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Net202131123002036Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
